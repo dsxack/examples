31b3d101413faa1c44fce6d7375d88142317f7ac
Smotrov Dmitriy <dsxack@gmail.com>
2018-07-19 19:26:10 +0300

    add rust-telegrambot

---
+++ rust-telegrambot/src/main.rs
@@ -0,0 +1,38 @@
+extern crate futures;
+extern crate telegram_bot;
+extern crate tokio_core;
+
+use std::env;
+
+use futures::Stream;
+use tokio_core::reactor::Core;
+use telegram_bot::*;
+
+fn main() {
+    let mut core = Core::new().unwrap();
+
+    let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
+    let api = Api::configure(token).build(core.handle()).unwrap();
+
+    // Fetch new updates via long poll method
+    let future = api.stream().for_each(|update| {
+
+        // If the received update contains a new message...
+        if let UpdateKind::Message(message) = update.kind {
+
+            if let MessageKind::Text {ref data, ..} = message.kind {
+                // Print received text message to stdout.
+                println!("<{}>: {}", &message.from.first_name, data);
+
+                // Answer message with "Hi".
+                api.spawn(message.text_reply(
+                    format!("Hi, {}! You just wrote '{}'", &message.from.first_name, data)
+                ));
+            }
+        }
+
+        Ok(())
+    });
+
+    core.run(future).unwrap();
+}
