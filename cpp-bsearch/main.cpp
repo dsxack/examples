148d8d4cbba0b7f101ac1b4c75763288da72afaf
Smotrov Dmitriy <dsxack@gmail.com>
2018-08-06 01:14:45 +0300

    add cpp-bsearch

---
+++ cpp-bsearch/main.cpp
@@ -0,0 +1,46 @@
+#include "string.h"
+#include "stdio.h"
+
+#define NELEMS(array) (sizeof(array) / sizeof(array[0]))
+
+struct Nameval {
+    const char *name;
+    int     value;
+};
+
+/* lookup: двоичный поиск строки name в массиве tab; возвращает индекс */
+int lookup(const char *name, struct Nameval tab[], int ntab) {
+    int low, high, mid, cmp;
+
+    low = 0;
+    high = ntab - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        cmp = strcmp(name, tab[mid].name);
+        if (cmp < 0) {
+            high = mid - 1;
+        } else if (cmp > 0) {
+            low = mid + 1;
+        } else { /* элемент найден */
+            return mid;
+        }
+    }
+
+    return -1; /* элемент не найден */
+}
+
+int main() {
+    struct Nameval htmlchars[] = {
+        "AElig",    0x00c6,
+        "AAcute",   0x00c1,
+        "Acicrc",   0x00c2,
+        /* ... */
+        "zeta",     0x03b6,
+    };
+
+    int index = lookup("zeta", htmlchars, NELEMS(htmlchars));
+
+    printf("%x", htmlchars[index].value);
+
+    return 0;
+}
