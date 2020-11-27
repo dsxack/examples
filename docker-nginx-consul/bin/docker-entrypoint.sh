7bcb2ca983ea928abe9b8b6a1caa20ea1a91b21e
Smotrov Dmitriy <dsxack@gmail.com>
2020-11-27 19:01:48 +0300

    Fix docker-nginx-consul example

--- docker-nginx-consul/bin/docker-entrypoint.sh
+++ docker-nginx-consul/bin/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 nginx -g "daemon off;" \
-	& consul-template -template "/etc/nginx/conf.d/default.conf.ctmpl:/etc/nginx/conf.d/default.conf:nginx -s reload"
+	& consul-template -consul-addr $CONSUL_ADDR -template "/etc/nginx/conf.d/default.conf.ctmpl:/etc/nginx/conf.d/default.conf:nginx -s reload"
